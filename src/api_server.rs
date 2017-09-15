3569afaa75416603e4e3c70c653b61ee4d4b9103
Dominik Winecki <dominikwinecki@gmail.com>
2017-09-15 19:16:17 -0400

    Add iron http api

--- src/api_server.rs
+++ src/api_server.rs
@@ -1,3 +1,10 @@
+extern crate iron;
+use self::iron::prelude::*;
+use self::iron::status;
+
 pub fn start() {
     info!("Starting API server");
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello world!")))
+    }).http("localhost:3000").unwrap();
 }
