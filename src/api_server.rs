621a25ca8e15ed7818195ff7064d36e206c4cbc1
Dominik Winecki <dominikwinecki@gmail.com>
2017-09-15 19:07:57 -0400

    Remove hyper http api

--- src/api_server.rs
+++ src/api_server.rs
@@ -1,25 +1,3 @@
-use hyper::header::ContentLength;
-use hyper::server::{Http, Request, Response, Service};
-
 pub fn start() {
-    let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(APIServiceTest)).unwrap();
-    server.run().unwrap();
-}
-
-struct APIServiceTest;
-
-impl Service for APIServiceTest {
-    type Request = Request;
-    type Response = Response;
-    type Error = ::hyper::Error;
-    type Future = ::futures::future::FutureResult<Self::Response, Self::Error>;
-
-    fn call(&self, _req: Request) -> Self::Future {
-        info!("Recieved request from {}", _req.remote_addr().unwrap());
-        let out = "Hello";
-        let rsp = Response::new().with_header(ContentLength(out.len() as u64)).
-            with_body(out);
-        ::futures::future::ok(rsp)
-    }
+    info!("Starting API server");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-extern crate hyper;
-extern crate futures;
 
 mod config;
 mod db;
